6bbf541476746f4269588944a578da774c03a8c3
Денис <denisegorenko1@gmail.com>
2022-11-27 22:17:33 +0300

    3 lesson v11

--- src/middlewares/authorisation-middleware.ts
+++ src/middlewares/authorisation-middleware.ts
@@ -9,18 +9,16 @@ export const authorisationMiddleware = (req: Request, res: Response, next: NextF
 
     if (authType !== 'Basic') {
         res.status(401).send('Authentication wrong.')
+        return
     }
 
     const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
 
     const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
 
-
-    if (!login && password && login === auth.login && password === auth.password) {
+    if (!(login && password && login === auth.login && password === auth.password)) {
         res.status(401).send('Authentication required.')
-        return
     } else {
         next()
     }
-
 }
