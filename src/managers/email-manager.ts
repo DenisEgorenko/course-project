558756d376ad9c0c3a98db04c6f721e6dcdedf75
Денис <denisegorenko1@gmail.com>
2022-12-16 22:10:49 +0300

    7 lesson v1

--- src/managers/email-manager.ts
+++ src/managers/email-manager.ts
@@ -6,7 +6,7 @@ export const EmailManager = {
         await EmailAdapter.sendEmail(user.accountData.email, 'Registration Confirmation',
             `<h1>Thank for your registration</h1>
        <p>To finish registration please follow the link below:
-          <a href='https://somesite.com/confirm-email?${user.emailConfirmation.confirmationCode}'>complete registration</a>
+          <a href='https://somesite.com/confirm-email?code=${user.emailConfirmation.confirmationCode}'>complete registration</a>
       </p>`
         )
     }
